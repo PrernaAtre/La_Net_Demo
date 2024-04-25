2a68f93a9235d132e611a71122c789dee5872baa
prernaatre <prernaatre11@gmail.com>
2024-04-25 23:58:35 +0530

    added try edior

---
+++ src/app/routes/editor/page.tsx
@@ -0,0 +1,11 @@
+import EditorTry from '@/app/(main)/_components/editorTry'
+import MainLayout from '@/app/(main)/layout'
+import React from 'react'
+function editorPage () {
+  return (
+    <MainLayout>
+    <EditorTry />
+    </MainLayout>
+  )
+}
+export default editorPage;
