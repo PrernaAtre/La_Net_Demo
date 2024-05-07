c3eb3114aa8627ee608ffa5a301d7a5306d77e3d
PrernaAtre <prernaatre11@gmail.com>
2024-05-07 14:15:34 +0530

    Refactor QuickNoteService to use environment variable for OpenAI API key

--- src/quick-note/quick-note.service.ts
+++ src/quick-note/quick-note.service.ts
@@ -8,55 +8,55 @@ import OpenAI from "openai";
 
 @Injectable()
 export class QuickNoteService {
-    constructor(
-        @InjectModel('QuickNote') private quickNoteModel: Model<QuickNote>) { }
+  constructor(
+    @InjectModel('QuickNote') private quickNoteModel: Model<QuickNote>) { }
 
-    async createQuickNote(userId: string, title: string): Promise<QuickNote> {
-        console.log(" user id : ", userId)
-        console.log("title : ", title)
-        console.log("key ----------",process.env.OPEN_AI_KEY);
-        
-        // Create a new quick note instance
-        // const openAI = new OpenAIApi(new Configuration({
-        //     apiKey: process.env.OPENAI_API_KEY,
-        //   }));
-        const openAI = new OpenAI(({
-            apiKey: "sk-proj-60x0CdwK7iLj78A1iSs2T3BlbkFJ3JL2XVMknw7CQjwI7xJa",
-          }));
-        const completion = await openAI.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages:[{ role: "user", content: title }],
-        });
-      //  console.log(completion);
-        console.log(completion.choices[0].message.content);
-        const createdNote = new this.quickNoteModel({
-            title,
-            userId,
-            description : completion.choices[0].message.content
-        });
-        // Save the new quick note to the database
-        return await createdNote.save();
-    }
+  async createQuickNote(userId: string, title: string): Promise<QuickNote> {
+    console.log(" user id : ", userId)
+    console.log("title : ", title)
+    console.log("key ----------", process.env.OPEN_AI_KEY);
 
-    async generateResponse(userMessage: string) {
-        console.log("userMessage---", userMessage);
-        
-        const openAI = new OpenAI(({
-            apiKey: "sk-proj-60x0CdwK7iLj78A1iSs2T3BlbkFJ3JL2XVMknw7CQjwI7xJa",
-          }));
-        const completion = await openAI.chat.completions.create({
-          messages: [
-            { role: 'system', content: 'You are a helpful assistant.' },
-            { role: 'user', content: userMessage },
-          ],
-          model: 'gpt-3.5-turbo',
-        });
-        const createdEmail = new this.quickNoteModel({
-            title : userMessage,
-            userId : "123456",
-            description : completion.choices[0].message.content
-        });
-        await createdEmail.save();
-        return completion.choices[0].message.content;
-      }
+    // Create a new quick note instance
+    // const openAI = new OpenAIApi(new Configuration({
+    //     apiKey: process.env.OPENAI_API_KEY,
+    //   }));
+    const openAI = new OpenAI(({
+      apiKey: process.env.OPEN_AI_KEY,
+    }));
+    const completion = await openAI.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: title }],
+    });
+    //  console.log(completion);
+    console.log(completion.choices[0].message.content);
+    const createdNote = new this.quickNoteModel({
+      title,
+      userId,
+      description: completion.choices[0].message.content
+    });
+    // Save the new quick note to the database
+    return await createdNote.save();
+  }
+
+  async generateResponse(userMessage: string) {
+    console.log("userMessage---", userMessage);
+
+    const openAI = new OpenAI(({
+      apiKey: process.env.OPEN_AI_KEY,
+    }));
+    const completion = await openAI.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: userMessage },
+      ],
+      model: 'gpt-3.5-turbo',
+    });
+    const createdEmail = new this.quickNoteModel({
+      title: userMessage,
+      userId: "123456",
+      description: completion.choices[0].message.content
+    });
+    await createdEmail.save();
+    return completion.choices[0].message.content;
+  }
 }
